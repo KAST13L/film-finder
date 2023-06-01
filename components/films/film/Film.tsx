6b46e04cce516675cba807b244c600eaefae6c5d
KAST13L <o4enbsmeshno@gmail.com>
2023-06-01 20:30:42 +0300

    fix: update styles

--- components/films/film/Film.tsx
+++ components/films/film/Film.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { FilmType } from "@/components/films/Films";
 import styles from "./film.module.scss";
-import { useState } from "react";
+import React, { useState } from "react";
+import { Rating } from "@mui/material";
 
 type PropsType = {
   film: FilmType;
@@ -12,9 +13,22 @@ export const Film = ({ film }: PropsType) => {
 
   return (
     <div className={styles.film} onBlur={() => setIsShowDetails(false)}>
-      <img src={image ? image.medium : "a"} alt={name} />
-      <div>Name: {name}</div>
-      <div>Ratting: {rating.average ? rating.average : "unknown"}</div>
+      <img
+        src={
+          image
+            ? image.original
+            : "https://images.moviesanywhere.com/3b9542164920b044dff30500da3266f7/5d5cca7a-9603-4300-aac1-96577d784dd9.jpg"
+        }
+        alt={name}
+      />
+      <div className={styles.title}>{name}</div>
+      <Rating
+        name="read-only"
+        precision={0.1}
+        value={rating.average ? rating.average / 2 : 0}
+        readOnly
+      />
+      {!rating.average && <div>the rating is unknown</div>}
 
       {isShowDetails ? (
         <div>
