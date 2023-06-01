f1127551d44d2eec9630218b0e5f031e0325bac4
KAST13L <o4enbsmeshno@gmail.com>
2023-06-01 21:51:46 +0300

    fix: some some

--- components/films/film/Film.tsx
+++ components/films/film/Film.tsx
@@ -11,43 +11,54 @@ export const Film = ({ film }: PropsType) => {
   const [isShowDetails, setIsShowDetails] = useState(false);
   const { name, image, rating, genres, status, summary, schedule } = film.show;
 
+  const imageContent = !isShowDetails && (
+    <img
+      src={
+        image
+          ? image.original
+          : "https://images.moviesanywhere.com/3b9542164920b044dff30500da3266f7/5d5cca7a-9603-4300-aac1-96577d784dd9.jpg"
+      }
+      alt={name}
+    />
+  );
+
+  const scheduleMessage = (
+    <div>
+      Schedule:{" "}
+      {schedule.time || schedule.days.length
+        ? schedule.time + " " + schedule.days.join(", ")
+        : "unknown"}
+    </div>
+  );
+
+  const summaryMessage = (
+    <div>
+      Summary: {summary ? summary.replace(/(\<(\/?[^>]+)>)/g, "") : "unknown"}
+    </div>
+  );
+
   return (
     <div className={styles.film} onBlur={() => setIsShowDetails(false)}>
-      <img
-        src={
-          image
-            ? image.original
-            : "https://images.moviesanywhere.com/3b9542164920b044dff30500da3266f7/5d5cca7a-9603-4300-aac1-96577d784dd9.jpg"
-        }
-        alt={name}
-      />
+      {imageContent}
+
       <div className={styles.title}>{name}</div>
+
       <Rating
         name="read-only"
         precision={0.1}
         value={rating.average ? rating.average / 2 : 0}
         readOnly
       />
-      {!rating.average && <div>the rating is unknown</div>}
 
-      {isShowDetails ? (
+      {isShowDetails && (
         <div>
           <div>Genres: {genres.length ? genres.join(", ") : "unknown"}</div>
           <div>Status: {status}</div>
-          <div>
-            Schedule:{" "}
-            {schedule.time || schedule.days.length
-              ? schedule.time + " " + schedule.days.join(", ")
-              : "unknown"}
-          </div>
-          <div>
-            Summary:{" "}
-            {summary ? summary.replace(/(\<(\/?[^>]+)>)/g, "") : "unknown"}
-          </div>
+          {scheduleMessage}
+          {summaryMessage}
         </div>
-      ) : (
-        <></>
       )}
+
       <button onClick={() => setIsShowDetails(!isShowDetails)}>
         {isShowDetails ? "hide details" : "show details"}
       </button>
